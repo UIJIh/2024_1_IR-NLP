90719390cef7289075370df0c320450ce3e176b3
Uiji <116754983+UIJIh@users.noreply.github.com>
2024-05-15 20:16:56 +0900

    Delete 1/master/utils/.ipynb_checkpoints directory

--- 1/master/utils/.ipynb_checkpoints/effect-checkpoint.py
+++
@@ -1,6 +0,0 @@
-import random
-
-def flash_text(button):
-    color = random.choice(['red', 'green', 'blue', 'purple'])
-    button.config(fg=color)
-    button.after(500, lambda: flash_text(button)) 
